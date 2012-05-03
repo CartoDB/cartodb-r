bc6116f10cefc10f538c98c348fa3ff1af10a8ea
Andrew W Hill <andrewxhill@gmail.com>
2012-05-03 13:25:05 -0400

    more doc for Readme

--- CartoDB/R/cartodb.collection.R
+++ CartoDB/R/cartodb.collection.R
@@ -1,17 +1,24 @@
 cartodb.collection <-
-function(name = NULL, columns = NULL, geomAs = "XY", limit = NULL, sql = NULL, asJson = FALSE) {
+function(name = NULL, columns = NULL, geomAs = NULL, limit = NULL, sql = NULL, asJson = FALSE) {
     if (is.character(name)){
         # Method to handle geomAs parameter
         geomCol <- function(option) { 
-            if (option=="XY") { return('ST_X(the_geom) AS the_geom_x, ST_Y(the_geom) AS the_geom_y,null as the_geom') }
+            if (is.null(option)) { return('the_geom') }
+            else if (option=="XY") { return('ST_X(the_geom) AS the_geom_x, ST_Y(the_geom) AS the_geom_y,null as the_geom') }
             else if (option=="WKT") { return('ST_AsText(the_geom) AS the_geom') }
             else if (option=="GeoJSON") { return('ST_AsGeoJSON(the_geom) AS the_geom') }
             else if (option=="WKB") { return('the_geom as the_geom') }
             else if (option=="the_geom") { return('the_geom') }
-            else if (is.null(option)) { return(NULL) }
             else { return('ST_X(the_geom) AS the_geom_x, ST_Y(the_geom) AS the_geom_y,null as the_geom') }
         }
         if (is.list(columns)){
+            # replace the_geom with a processed version if asked for
+            if ( 'the_geom' %in% columns) {
+                if(!is.null(geomAs) && geomAs!="the_geom"){
+                    replace("the_geom",columns,geomCol(geomAs))
+                }
+            }
+            
             sql <- paste("SELECT ", paste(columns, collapse=","), " FROM ", name,sep='')
         } else {
             sql <- paste("SELECT *, NULL as the_geom_webmercator,",geomCol(geomAs)," FROM ", name,sep='')
@@ -27,12 +34,15 @@ function(name = NULL, columns = NULL, geomAs = "XY", limit = NULL, sql = NULL, a
             cartodb.collection.json<-fromJSON(cartodb.collection.get[[1]])
             if ( 'rows' %in% names(cartodb.collection.json)) {
                 df <- jsonToDataFrame(cartodb.collection.json$rows)
+                
                 # remove null columns from geom transformations
-                if (is.character(name)){
-                    if(geomAs!="XY"){
-                        df <- df[,!(colnames(df) %in% c("the_geom_webmercator"))]
-                    } else {
-                        df <- df[,!(colnames(df) %in% c("the_geom","the_geom_webmercator"))]
+                if (!is.null(name) & is.character(name)){
+                    if(!is.list(columns)) {
+                        if(is.null(geomAs) || geomAs!="XY"){
+                            df <- df[,!(colnames(df) %in% c("the_geom_webmercator"))]
+                        } else {
+                            df <- df[,!(colnames(df) %in% c("the_geom","the_geom_webmercator"))]
+                        }
                     }
                 }
                 return(df)
