535dee08a2bba1ab4a68d23b1408f1df2557f37d
Andrew W Hill <andrewxhill@gmail.com>
2012-05-23 13:37:12 -0400

    optimized the spatial distance matrix SQL call and added the HULL option

--- CartoDB/R/cartodb.spatial.R
+++ CartoDB/R/cartodb.spatial.R
@@ -1,17 +1,23 @@
 cartodb.spatial.dm <-
-function(name = NULL, method="CENTROID", limit=100) {
+function(name = NULL, method="CENTROID", limit=NULL) {
     warning('CartoDB Distance Matrix support is experimental')
     if (is.character(name)){
         url <- cartodb.sql.base()
         if (method=="EXACT"){
-            the_geom = "the_geom::geography"
+            the_geom = "the_geom"
         } else if (method=="CENTROID") {
-            the_geom = "ST_Centroid(the_geom)::geography"
+            the_geom = "ST_Centroid(the_geom)"
+        } else if (method=="HULL") {
+            the_geom = "ST_ConvexHull(the_geom)"
         } else {
             warning("method not recognized, defaulting to CENTROID")
-            the_geom = "ST_Centroid(the_geom)::geography"
+            the_geom = "ST_Centroid(the_geom)"
         }
-        sql <- paste("with foo as (SELECT cartodb_id,",the_geom," geo FROM ",name," LIMIT ",limit,") SELECT a.cartodb_id cartodb_a,b.cartodb_id cartodb_id_b,st_distance(a.geo,b.geo) meters FROM foo a CROSS JOIN foo b WHERE a.cartodb_id < b.cartodb_id",sep="")
+        lim = ""
+        if (is.numeric(limit)){
+            lim = paste(" LIMIT ",limit,sep="")
+        }
+        sql <- paste("with foo as (SELECT cartodb_id,",the_geom," geo FROM ",name,lim,") SELECT a.cartodb_id cartodb_a,b.cartodb_id cartodb_id_b,st_distance_sphere(a.geo,b.geo) meters FROM foo a JOIN foo b ON a.cartodb_id < b.cartodb_id",sep="")
         url <- URLencode(paste(url,"q=",sql,sep=''))
         df<-cartodb.df(sql)
         if ( !is.null(df) ) {
